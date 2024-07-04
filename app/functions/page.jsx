83b365c38fb455dcff99af3f7a0e6bf072d8b1da
Muhammad zeeshan <fullstackdeveloper486@gmail.com>
2024-07-04 18:06:47 +0500

    Functions Describe

---
+++ app/functions/page.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Functions = () => {
+  // example of function expression
+  function Student() {
+    console.log("Hi i am student of tuf university");
+  }
+  Student();
+  //   example of Anonumous function
+  // call back function is similar with anonumous function
+  const student2 = function anonumousfun(one, two) {
+    return one + two;
+  };
+  student2(2, 3);
+
+  //example of arrow function
+
+  //   basic arrow function
+  const arrowfun = (a, b) => {
+    console.log(`${a} : ${b}`);
+  };
+  arrowfun(5, 10);
+
+  const arrowfunc = (a, b) => console.log(`${a}, author: ${b}`);
+  arrowfunc(
+    "agr block of scope mein aik line hy then hum arrow function ki brackets ko remove kr sekhty hein",
+    "arslan"
+  );
+
+  //   agr aik parameter hy then we can remove paranthisis
+  const arrowfunct = (tufstudent) => console.log(`${tufstudent}`);
+  arrowfunct("omiad");
+  return (
+    <div>
+      <h2>Types of Functions</h2>
+      <li>Function Expression</li>
+      <li>Anonumous Function</li>
+      <li>Arrow Function</li>
+    </div>
+  );
+};
+
+export default Functions;
